6f9dc45644738b251bb4f630763c3123182bf318
Jeremy Van Patten <vanpattenjeremy@gmail.com>
2022-09-11 22:25:11 -0600

    Initial project creation

---
+++ TacticsGame/Intermediate/Build/Win64/TacticsGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30146
+#include "D:/UnrealProjects/BasicTacticsGame/TacticsGame/Intermediate/Build/Win64/TacticsGameEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ TacticsGame/Intermediate/Build/Win64/UnrealEditor/Inc/TacticsGame/TacticsGameGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef TACTICSGAME_TacticsGameGameModeBase_generated_h
+#error "TacticsGameGameModeBase.generated.h already included, missing '#pragma once' in TacticsGameGameModeBase.h"
+#endif
+#define TACTICSGAME_TacticsGameGameModeBase_generated_h
+
+#define FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_SPARSE_DATA
+#define FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_RPC_WRAPPERS
+#define FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesATacticsGameGameModeBase(); \
+	friend struct Z_Construct_UClass_ATacticsGameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATacticsGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/TacticsGame"), NO_API) \
+	DECLARE_SERIALIZER(ATacticsGameGameModeBase)
+
+
+#define FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesATacticsGameGameModeBase(); \
+	friend struct Z_Construct_UClass_ATacticsGameGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(ATacticsGameGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/TacticsGame"), NO_API) \
+	DECLARE_SERIALIZER(ATacticsGameGameModeBase)
+
+
+#define FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATacticsGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATacticsGameGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATacticsGameGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATacticsGameGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATacticsGameGameModeBase(ATacticsGameGameModeBase&&); \
+	NO_API ATacticsGameGameModeBase(const ATacticsGameGameModeBase&); \
+public:
+
+
+#define FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ATacticsGameGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ATacticsGameGameModeBase(ATacticsGameGameModeBase&&); \
+	NO_API ATacticsGameGameModeBase(const ATacticsGameGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ATacticsGameGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ATacticsGameGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ATacticsGameGameModeBase)
+
+
+#define FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_12_PROLOG
+#define FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_SPARSE_DATA \
+	FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_RPC_WRAPPERS \
+	FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_INCLASS \
+	FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_SPARSE_DATA \
+	FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> TACTICSGAME_API UClass* StaticClass<class ATacticsGameGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_TacticsGame_Source_TacticsGame_TacticsGameGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
